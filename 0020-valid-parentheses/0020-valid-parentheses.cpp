69e53330c70831627ec38e91854682c6c8bf0114
Mustafa TÜRK <70493201+MtrkJmL@users.noreply.github.com>
2023-12-26 02:41:39 +0100

    Time: 3 ms (45.20%), Space: 6.8 MB (36.62%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> p;
+        for(int i=0; i<s.size();i++){
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{')
+                p.push(s[i]);
+            else if(!p.empty()){
+                if(s[i]==')'){
+                    if(p.top()=='(')
+                        p.pop();
+                    else return false;
+                }
+                else if(s[i]==']'){
+                    if(p.top()=='[')
+                        p.pop();
+                    else return false;
+                }
+                else if(s[i]=='}'){
+                    if(p.top()=='{')
+                        p.pop();
+                    else return false;
+                }
+            }
+            else return false;
+        }
+        if(p.size()==0)
+            return true;
+        else return false;
+    }
+};
