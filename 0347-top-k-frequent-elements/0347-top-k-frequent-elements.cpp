129b5f22d20609d8af015223dbb30b95a5e73d1e
Mustafa TÜRK <70493201+MtrkJmL@users.noreply.github.com>
2025-02-06 19:37:40 +0100

    Time: 6 ms (23.75%), Space: 20.1 MB (6.33%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int> hash;
-        vector<int> result;
-        for(int i=0; i<nums.size(); i++){
-            hash[nums[i]]++;
+        unordered_map<int,int>s;
+        vector<int> res;
+        for(int i=0;i<nums.size();i++){
+            s[nums[i]]++;
+        }
+        vector<vector<int>> hash(nums.size()+1);
+        for(auto j=s.begin(); j!=s.end();j++){
+            hash[j->second].push_back(j->first);
         }
         
-        vector<vector<int>> hashVal(nums.size()+1);
-        for(auto j=hash.begin(); j!=hash.end(); j++){
-            hashVal[j->second].push_back(j->first);
-        }
-        
-        for(int i=nums.size(); i>=0;i--){
-            if(!hashVal[i].empty()){
-                if(result.size()>=k)
-                    break;
-                result.insert(result.end(),hashVal[i].begin(),hashVal[i].end());
+        for(int i=nums.size();i>=0;i--){
+            if(res.size()>=k)
+                break;
+            if(!hash[i].empty()){
+                res.insert(res.end(),hash[i].begin(),hash[i].end());
             }
         }
-        return result;
+        return res;
     }
 };
